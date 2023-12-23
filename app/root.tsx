d573054d36df72ecfa3f8fe86b73570daffaa156
Fahad Ahmed <fahad.ahmed@me.com>
2023-12-23 20:13:06 +0530

    Revert "removing sass and @carbon/react package"
    This reverts commit 5ccd8ec13b508144ab3e864f108300ca879184df.

--- app/root.tsx
+++ app/root.tsx
@@ -8,10 +8,12 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from './styles/global.css';
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [
     { rel: "stylesheet", href: cssBundleHref },
+    { rel: "stylesheet", href: styles },
   ] : []),
 ];
 
